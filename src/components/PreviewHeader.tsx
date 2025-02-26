e6826da04b1f0139cb00641116a1b3153c906796
Felix Pacheco <devfex.19@gmail.com>
2025-02-26 12:10:00 -0400

    Add PreviewHeader component for displaying header with CircleIcons

---
+++ src/components/PreviewHeader.tsx
@@ -0,0 +1,9 @@
+import { CircleIcon } from './CircleIcon'
+
+export const PreviewHeader = () => (
+    <header className="text-neutral-400 flex items-center gap-1 mb-3">
+        <CircleIcon />
+        <CircleIcon />
+        <CircleIcon />
+    </header>
+)
