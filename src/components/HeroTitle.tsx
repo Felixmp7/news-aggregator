c471e10e54c19c4be0282288c75832f91195f505
Felix Pacheco <devfex.19@gmail.com>
2025-02-26 12:10:59 -0400

    Add HeroTitle component for displaying the main title and description of the news aggregator

---
+++ src/components/HeroTitle.tsx
@@ -0,0 +1,8 @@
+import { cn } from "@/lib/utils";
+
+export const HeroTitle = ({ className }: { className?: string }) => (
+    <section className={cn("w-full h-screen flex flex-col justify-end container mx-auto pb-5", className)}>
+        <p className="text-base sm:text-xl md:text-3xl xl:text-5xl text-balance opacity-40">The challenge is to create the user interface for a news aggregator website that pulls articles from various sources and displays them in a clean, easy-to-read format.</p>
+        <h1 className="text-5xl sm:text-6xl md:text-9xl xl:text-[10rem] font-semibold">News Aggregator</h1>
+    </section>
+)
