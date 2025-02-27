18f2cab52aff366048d4019bc7587a03a88b5f1d
Felix Pacheco <devfex.19@gmail.com>
2025-02-27 11:40:54 -0400

    Refactor utility functions and add date formatting helper

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
 import { clsx, type ClassValue } from "clsx"
+import { format } from "date-fns"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs))
+export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
+
+/**
+ *
+ * @param date string
+ * @returns string | null
+ * @example formatDate('2022-01-01T00:00:00Z') // 'January 1, 2022 12:00 AM'
+ */
+export const formatDate = (date?: string) => {
+    if (date) return format(new Date(date), 'PPpp')
+    return null
 }
