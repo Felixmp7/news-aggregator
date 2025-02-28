2e2b538df9b186673328858de81a09f59960e159
Felix Pacheco <devfex.19@gmail.com>
2025-02-28 11:48:54 -0400

    Add New York Times API key to api.constants.ts

--- src/constants/api.constants.ts
+++ src/constants/api.constants.ts
@@ -1,2 +1,3 @@
 export const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 export const THE_GUARDIAN_API_KEY = import.meta.env.VITE_THE_GUARDIAN_API_KEY;
+export const NEW_YORK_TIMES_API_KEY = import.meta.env.VITE_NEW_YORK_TIMES_API_KEY;
