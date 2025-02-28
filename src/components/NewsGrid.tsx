611d4202b02b26d762a2de78705bbf10ce0de73c
Felix Pacheco <devfex.19@gmail.com>
2025-02-28 10:54:41 -0400

    Refactor NewsGrid component to improve loading and article rendering logic

--- src/components/NewsGrid.tsx
+++ src/components/NewsGrid.tsx
@@ -1,4 +1,4 @@
-import { NewsapiArticle } from "@/models/news.interfaces";
+import { NewsapiArticle } from "@/models/newsapi.types";
 import { PreviewHeader } from "./PreviewHeader";
 import { PreviewNew } from "./PreviewNew";
 
@@ -12,29 +12,36 @@ interface Props {
 
 export const NewsGrid = ({ isLoading, articles, onSelectArticle}: Props) => (
     <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 container mx-auto pb-10">
-        {isLoading ? (
-            SKELETON_LIST.map((index) => (
-                <div key={index} className="animate-pulse flex flex-col justify-between p-4 rounded-lg bg-neutral-100 h-60 text-neutral-600">
-                    <PreviewHeader />
-                    <div className="grow flex items-center justify-center bg-neutral-200 rounded-2xl mb-4" />
-                    <div className="flex justify-between items-center">
-                        <div className="w-16 h-4 bg-neutral-200 rounded-full" />
-                        <div className="w-16 h-4 bg-neutral-200 rounded-full" />
+        {(() => {
+            if (isLoading) {
+                return SKELETON_LIST.map((index) => (
+                    <div key={index} className="animate-pulse flex flex-col justify-between p-4 rounded-lg bg-neutral-100 h-60 text-neutral-600">
+                        <PreviewHeader />
+                        <div className="grow flex items-center justify-center bg-neutral-200 rounded-2xl mb-4" />
+                        <div className="flex justify-between items-center">
+                            <div className="w-16 h-4 bg-neutral-200 rounded-full" />
+                            <div className="w-16 h-4 bg-neutral-200 rounded-full" />
+                        </div>
                     </div>
-                </div>
-            ))
-        ) : (
-            articles?.map(({ title, source, urlToImage, publishedAt, ...rest }) => (
-                <PreviewNew
-                    key={title}
-                    imageAlt={`Image for article: ${title}`}
-                    title={title}
-                    imageSrc={urlToImage}
-                    source={source.name}
-                    date={publishedAt}
-                    onClick={() => onSelectArticle({ title, source, urlToImage, publishedAt, ...rest })}
-                />
-            ))
-        )}
+                ))
+            }
+            if (articles?.length) {
+                return articles
+                    .map(({ title, source, urlToImage, publishedAt, ...rest }) => (
+                        <PreviewNew
+                            key={title}
+                            imageAlt={`Image for article: ${title}`}
+                            title={title}
+                            imageSrc={urlToImage}
+                            source={source.name}
+                            date={publishedAt}
+                            onClick={() => onSelectArticle({ title, source, urlToImage, publishedAt, ...rest })}
+                        />
+                    ))
+            }
+            return (
+                <>No results</>
+            )
+        })()}
     </section>
 )
