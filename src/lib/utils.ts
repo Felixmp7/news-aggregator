448d7baa9f5b9d8e0785a3bd3ba47e29399df936
Felix Pacheco <devfex.19@gmail.com>
2025-02-28 20:16:57 -0400

    Add functions to manage query parameters and local storage for enhanced data retrieval

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,8 @@ import { clsx, type ClassValue } from "clsx"
 import { format } from "date-fns"
 import { twMerge } from "tailwind-merge"
 
+import { NewsAggregatorQueryParams } from "@/models/news-aggregator.types"
+
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
 
 /**
@@ -15,17 +17,60 @@ export const formatDate = (date?: string) => {
     return null
 }
 
-export const getCurrentParams = () => {
+export const getFavoritesFromLocalStorage = () => {
+    const source = localStorage.getItem('source')
+    const keywords =  localStorage.getItem('keywords')
+    const category =  localStorage.getItem('category')
+    const from = localStorage.getItem('from')
+    const to = localStorage.getItem('to')
+
+    return { keywords, from, to, category, source }
+}
+
+export const getQueryParams = () => {
     const queryParams = new URLSearchParams(window.location.search)
+
+    const source = queryParams.get('source')
     const keywords = queryParams.get('keywords')
     const from = queryParams.get('from')
     const to = queryParams.get('to')
     const category = queryParams.get('category')
-    return { keywords, from, to, category }
+
+    return {
+        keywords,
+        from,
+        to,
+        category,
+        source,
+        hasValues: (!!source || !!keywords || !!from || !!to || !!category)
+    }
+}
+
+export const getInitialValues = () : NewsAggregatorQueryParams => {
+    const { hasValues, ...values } = getQueryParams();
+    const valuesFromLocalStorage = getFavoritesFromLocalStorage();
+
+    if (hasValues) return values as unknown as NewsAggregatorQueryParams
+
+    return valuesFromLocalStorage as unknown as NewsAggregatorQueryParams
+}
+
+export const setQueryParams = ({ source, keywords, category, from, to }: NewsAggregatorQueryParams) => {
+    const queryParams = new URLSearchParams(window.location.search)
+    queryParams.set('source', source ?? '')
+    queryParams.set('keywords', keywords ?? '')
+    queryParams.set('category', category ?? '')
+    queryParams.set('from', from || '')
+    queryParams.set('to', to || '')
 }
 
 export const fetchDataSource = async <T>(url: string, init?: RequestInit) => {
     const response = await fetch(url, init)
     const data = await response.json()
     return data as T
+}
+
+export const pushQueryParams = (searchParams: string) => {
+    const newUrl = `${window.location.pathname}?${searchParams}`
+    window.history.pushState({}, '', newUrl)
 }
