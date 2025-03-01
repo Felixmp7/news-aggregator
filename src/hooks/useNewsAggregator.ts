2ea36ea8782059c5335a8f9a191211fee585bb18
Felix Pacheco <devfex.19@gmail.com>
2025-02-28 20:17:11 -0400

    Refactor useNewsAggregator hook to enhance state management and data fetching, including support for local storage and initial values

--- src/hooks/useNewsAggregator.ts
+++ src/hooks/useNewsAggregator.ts
@@ -1,27 +1,21 @@
-import { useState } from "react"
+import queryString from 'query-string';
+import { useEffect, useState } from "react";
+import { DateRange } from "react-day-picker";
 
-import { Article, Source } from "@/models/news-aggregator.types"
-import { getDataFromNYTimesSource } from "@/services/new-york-times.service"
-import { getDataFromNewsApiSource } from "@/services/newsapi.service"
-import { getDataFromTheGuardiansApiSource } from "@/services/the-guardian.service"
-import { useDateRangePicker } from "./useDateRangePicker"
-import { useQuery } from "./useQuery"
-import { useUrlSearchParams } from "./useUrlSearchParams"
+import { getInitialValues, pushQueryParams, setQueryParams } from "@/lib/utils";
+import { Article, NewsAggregatorQueryParams, Source } from "@/models/news-aggregator.types";
+import { getDataFromNYTimesSource } from "@/services/new-york-times.service";
+import { getDataFromNewsApiSource } from "@/services/newsapi.service";
+import { getDataFromTheGuardiansApiSource } from "@/services/the-guardian.service";
+import { useQuery } from "./useQuery";
 
 export const useNewsAggregator = () => {
     const [articleSelected, setArticleSelected]= useState<Article>()
     const [keywordValues, setKeywordValues]= useState('')
-    const [category, setCategory] = useState('')
+    const [categorySelected, setCategorySelected] = useState('')
     const [sourceSelected, setSourceSelected] = useState<Source>('news-api')
+    const [dateRangeSelected, setDateRangeSelected] = useState<DateRange>()
 
-    const { dateRange, handleChangeRange } = useDateRangePicker();
-
-    const { search } = useUrlSearchParams({
-        keywords: keywordValues,
-        source: sourceSelected,
-        category: category,
-        dateRange: dateRange
-    });
     const {
         data: newsData,
         isLoading: isLoadingNewsApi,
@@ -40,18 +34,49 @@ export const useNewsAggregator = () => {
         fetch: fetchNYTimes
     } = useQuery(getDataFromNYTimesSource)
 
-    const handleClickSearch = () => {
-        search();
-        if (sourceSelected === 'news-api') fetchNewsApi()
-        if (sourceSelected === 'guardian') fetchGuardians()
-        if (sourceSelected === 'new-york-times') fetchNYTimes()
-        return
-    }
-
     const handleChangeSource = (source: Source) => setSourceSelected(source)
     const handleChangeKeywords = (keywords: string) => setKeywordValues(keywords)
-    const handleChangeCategory = (category: string) => setCategory(category)
+    const handleChangeCategory = (category: string) => setCategorySelected(category)
     const handleSelectArticle = (article?: Article) => setArticleSelected(article || undefined)
+    const handleChangeRange = (dateRange: DateRange | undefined) => setDateRangeSelected(dateRange || undefined)
+
+    const fetchBySource = (values: NewsAggregatorQueryParams) => {
+        if (sourceSelected === 'news-api') fetchNewsApi(values)
+        if (sourceSelected === 'guardian') fetchGuardians(values)
+        if (sourceSelected === 'new-york-times') fetchNYTimes(values)
+    }
+
+    const handleClickSearch = () => {
+        const values = {
+            category: categorySelected,
+            from: dateRangeSelected?.from?.toISOString() || null,
+            to: dateRangeSelected?.to?.toISOString() || null,
+            keywords: keywordValues,
+            source: sourceSelected
+        }
+        setQueryParams(values);
+        pushQueryParams(queryString.stringify(values))
+        fetchBySource(values)
+    }
+
+    const handleSaveFiltersInLocalStorage = () => {
+        if (sourceSelected) localStorage.setItem('source', sourceSelected)
+        if (keywordValues) localStorage.setItem('keywords', keywordValues)
+        if (categorySelected) localStorage.setItem('category', categorySelected)
+        if (dateRangeSelected?.from) localStorage.setItem('from', dateRangeSelected.from.toISOString())
+        if (dateRangeSelected?.to) localStorage.setItem('to', dateRangeSelected.to.toISOString())
+    }
+
+    const fetchWithInitialValues = (initialValues: NewsAggregatorQueryParams) => {
+        if (initialValues.source) setSourceSelected(initialValues.source as Source)
+        if (initialValues.keywords) setKeywordValues(initialValues.keywords)
+        if (initialValues.category) setCategorySelected(initialValues.category)
+        setDateRangeSelected({
+            from: initialValues.from ? new Date(initialValues.from): undefined,
+            to: initialValues.to ? new Date(initialValues.to) : undefined
+        })
+        fetchBySource(initialValues)
+    }
 
     const dataSource: Record<Source, Article[] | undefined> = {
         'news-api': newsData?.articles,
@@ -59,13 +84,23 @@ export const useNewsAggregator = () => {
         'new-york-times': nyTimesData?.articles
     }
 
+    useEffect(() => {
+        const initialValues = getInitialValues();
+        const mustFetchWithInitialData = Object.values(initialValues).some((value) => !!value)
+
+        if (mustFetchWithInitialData) {
+            fetchWithInitialValues(initialValues)
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return {
         sourceSelected,
         keywordValues,
-        category,
-        dateRange,
+        categorySelected,
+        dateRangeSelected,
         articleSelected,
-        articles: dataSource[sourceSelected] ?? [],
+        articles: sourceSelected ? dataSource[sourceSelected] : [],
         isLoadingNewsApi,
         isLoadingGuardian,
         isLoadingNYTimes,
@@ -74,6 +109,7 @@ export const useNewsAggregator = () => {
         handleChangeCategory,
         handleChangeRange,
         handleClickSearch,
-        handleSelectArticle
+        handleSelectArticle,
+        handleSaveFiltersInLocalStorage
     }
 }
