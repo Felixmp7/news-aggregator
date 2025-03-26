15ae7e2c0aba301bdd39656baf139e09eb805dec
Felix Pacheco <devfex.19@gmail.com>
2025-03-26 11:05:32 -0400

    Add error handling to News API data fetching service

--- src/services/newsapi.service.ts
+++ src/services/newsapi.service.ts
@@ -4,7 +4,7 @@ import queryString from 'query-string';
 import { NEWS_API_BASE_URL, NEWS_API_KEY } from "@/constants/index.constants";
 import { fetchDataSource } from '@/lib/utils';
 import { DateRangeString, NewsAggregatorQueryParams, NewsAggregatorResponse } from '@/models/news-aggregator.types';
-import { NewsapiResponse } from "@/models/newsapi.types";
+import type { NewsapiResponse } from "@/models/newsapi.types";
 
 const MAX_MONTH_RANGE_DIFFERENCE = 1;
 
@@ -29,29 +29,39 @@ const getAValidDateRange = (from: string | null |undefined, to: string | null |u
 }
 
 export const getDataFromNewsApiSource = async ({ category, from, to, keywords }: Partial<NewsAggregatorQueryParams>): Promise<NewsAggregatorResponse> => {
-    const paramsForResource = queryString.stringify({
-        q: keywords || undefined,
-        category: category || undefined,
-        ...getAValidDateRange(from, to),
-    })
+    try {
+        const paramsForResource = queryString.stringify({
+            q: keywords || undefined,
+            category: category || undefined,
+            ...getAValidDateRange(from, to),
+        })
 
-    const newsApiResponse =
+        const newsApiResponse =
         await fetchDataSource<NewsapiResponse>(`${NEWS_API_BASE_URL}&${paramsForResource}`, {
             headers: { 'X-Api-Key': NEWS_API_KEY }
         })
 
-    return {
-        status: newsApiResponse.status,
-        articles: newsApiResponse.articles.map(article => ({
-            title: article.title,
-            urlToImage: article.urlToImage ?? undefined,
-            author: article.author,
-            content: article.content,
-            description: article.description,
-            source: article.source.name,
-            url: article.url,
-            publishedAt: article.publishedAt,
-            category: category || 'general'
-        }))
+        if ('message' in newsApiResponse) throw new Error(newsApiResponse?.message)
+
+        return {
+            status: newsApiResponse.status,
+            articles: newsApiResponse.articles.map(article => ({
+                title: article.title,
+                urlToImage: article.urlToImage ?? undefined,
+                author: article.author,
+                content: article.content,
+                description: article.description,
+                source: article.source.name,
+                url: article.url,
+                publishedAt: article.publishedAt,
+                category: category || 'general'
+            }))
+        }
+    } catch (error) {
+        return {
+            status: 'error',
+            articles: [],
+            error: error as Error,
+        }
     }
 }
